8e5fba85ca19b3e094cd45e25aca9c4fe68191b1
yangtongchun <m15169658896@163.com>
2021-04-24 23:24:13 +0800

    并发版

--- main.go
+++ main.go
@@ -34,30 +34,39 @@ func HttpGet(url string) (result string, err error) {
 	return
 }
 
+func SpiderPage(i int, page chan<- int) {
+	url := "https://tieba.baidu.com/f?kw=%E7%BB%9D%E5%9C%B0%E6%B1%82%E7%94%9F&ie=utf-8&pn=" +
+		strconv.Itoa((i-1)*50)
+	fmt.Println(url)
+
+	// 爬取网页内容
+	result, err := HttpGet(url)
+	if err != nil {
+		fmt.Println("HttpGet err = ", err)
+		return
+	}
+
+	// 把内容写入到文件
+	fileName := "/Users/yangtongchun/Desktop/" + strconv.Itoa(i) + ".html"
+	f, err1 := os.Create(fileName)
+	if err1 != nil {
+		fmt.Println("os.Create err = ", err1)
+		return
+	}
+	f.WriteString(result)
+	f.Close()
+	page <- i
+}
+
 func DoWork(start, end int) {
 	fmt.Println("正在爬取", start, "到", end, "的页面")
+	page := make(chan int)
 	// 明确爬取的范围或者网址
 	for i := start; i <= end; i++ {
-		url := "https://tieba.baidu.com/f?kw=%E7%BB%9D%E5%9C%B0%E6%B1%82%E7%94%9F&ie=utf-8&pn=" +
-			strconv.Itoa((i-1)*50)
-		fmt.Println(url)
-
-		// 爬取网页内容
-		result, err := HttpGet(url)
-		if err != nil {
-			fmt.Println("HttpGet err = ", err)
-			continue
-		}
-
-		// 把内容写入到文件
-		fileName := strconv.Itoa(i) + ".html"
-		f, err1 := os.Create(fileName)
-		if err1 != nil {
-			fmt.Println("os.Create err = ", err1)
-			continue
-		}
-		f.WriteString(result)
-		f.Close()
+		go SpiderPage(i, page)
+	}
+	for i := start; i <= end; i++ {
+		fmt.Printf("第%d页爬取完毕\n", <-page)
 	}
 }
 
